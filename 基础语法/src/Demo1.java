f1a5989af3a3a67be0fc292e6d79a8e240f6c1db
SuSVIP <2012573847@qq.com>
2021-11-30 15:55:18 +0800

    test

---
+++ 基础语法/src/Demo1.java
@@ -0,0 +1,7 @@
+//起始之语
+public class Demo1{
+    public static void main(String args[]){
+        System.out.println("Hello,world!");
+    }
+
+}
---
+++ 基础语法/src/Demo2.java
@@ -0,0 +1,14 @@
+//基本类型
+public class Demo2 {
+    public static void main(String args[]){
+        //8大基本类型
+        byte a=1;
+        short b=1;
+        int c=4;
+        long d=28483763617271L;
+        float e=2.3f;
+        double f=3.14159265354;
+        char g='A';
+        boolean h=true;
+    }
+}
